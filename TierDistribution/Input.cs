5017d9c335e5c02b6e49e0efbd7b8ec5f452e716
Sander de With <rednas92@hotmail.com>
2023-04-17 20:50:44 +0200

    Reading data

--- TierDistribution/Input.cs
+++ TierDistribution/Input.cs
@@ -8,56 +8,134 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace TierDistribution
 {
     public static class Input
     {
-        private static readonly string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
-        private static readonly string ApplicationName = "TierDistribution";
 
+        //public static void ReadSheet2() {
 
-        public static void ReadSheet()
+        //    Axios.get("https://sheets.googleapis.com/v4/spreadsheets/14fm2C7bpPJ7EzGTBdYFXHqL7KpSHGCpf8ktuNgLpn9o/?key=AIzaSyAIpz0Rm9ywnetyh76B49uEesbo1jYQk6Y&includeGridData=true").then((res) =>
+        //    {
+        //        var data = res.data.sheets[0].data[0].rowData;
+        //    });
+
+        //}
+
+        public static async Task<List<List<string>>> ReadData(string spreadsheetId, string apiKey, int sheetIndex)
         {
-            var data = ReadData("AIzaSyBw_lIFMnJppjdhbKtNABGZQbjXS_lsDJw", "Testsheet!B1:C2");
-            foreach (var row in data)
+            using var client = new HttpClient();
+
+            var response = await client.GetAsync($"https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}/?key={apiKey}&includeGridData=true");
+
+            if (!response.IsSuccessStatusCode)
             {
-                foreach (var cell in row)
+                throw new Exception($"Failed to read data from Google Sheets API. Status code: {response.StatusCode}");
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(content);
+
+            var sheetsResponse = JsonConvert.DeserializeObject<GoogleSheetsApiResponse>(content);
+
+            if (sheetIndex >= sheetsResponse.Sheets.Count)
+            {
+                throw new Exception($"Invalid sheet index: {sheetIndex}");
+            }
+
+            var rowData = sheetsResponse.Sheets[sheetIndex].Data.RowData;
+
+            var data = new List<List<string>>();
+
+            foreach (var row in rowData)
+            {
+                var rowDataList = new List<string>();
+
+                foreach (var cell in row.Values)
                 {
-                    Console.Write("{0}\t", cell);
+                    rowDataList.Add(cell.FormattedValue ?? "");
                 }
-                Console.WriteLine();
+
+                data.Add(rowDataList);
             }
+
+            return data;
         }
 
-        public static IList<IList<Object>> ReadData(string spreadsheetId, string range)
+        private class GoogleSheetsApiResponse
         {
-            UserCredential credential;
-
-            using (var stream = new FileStream("client_secret_882081320166-teh8os6ppeli8drtrjseo448a4c9mf0v.apps.googleusercontent.com.json", FileMode.Open, FileAccess.Read))
-            {
-                string credPath = "token.json";
-
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-            }
-
-            var service = new SheetsService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName,
-            });
-
-            SpreadsheetsResource.ValuesResource.GetRequest request =
-                    service.Spreadsheets.Values.Get(spreadsheetId, range);
-
-            ValueRange response = request.Execute();
-
-            return response.Values;
+            public List<GoogleSheetsApiSheet> Sheets { get; set; }
         }
+
+        private class GoogleSheetsApiSheet
+        {
+            public GoogleSheetsApiSheetData Data { get; set; }
+        }
+
+        private class GoogleSheetsApiSheetData
+        {
+            public List<GoogleSheetsApiRow> RowData { get; set; }
+        }
+
+        private class GoogleSheetsApiRow
+        {
+            public List<GoogleSheetsApiCell> Values { get; set; }
+        }
+
+        private class GoogleSheetsApiCell
+        {
+            public string FormattedValue { get; set; }
+        }
+
+
+
+        //private static readonly string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        //private static readonly string ApplicationName = "TierDistribution";
+
+        //public static void ReadSheet()
+        //{
+        //    var data = ReadData("14fm2C7bpPJ7EzGTBdYFXHqL7KpSHGCpf8ktuNgLpn9o", "Testsheet!B1:C2");
+        //    foreach (var row in data)
+        //    {
+        //        foreach (var cell in row)
+        //        {
+        //            Console.Write("{0}\t", cell);
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
+
+        //public static IList<IList<Object>> ReadData(string spreadsheetId, string range)
+        //{
+        //    UserCredential credential;
+
+        //    using (var stream = new FileStream("client_secret_882081320166-teh8os6ppeli8drtrjseo448a4c9mf0v.apps.googleusercontent.com.json", FileMode.Open, FileAccess.Read))
+        //    {
+        //        string credPath = "token.json";
+
+        //        credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+        //            GoogleClientSecrets.Load(stream).Secrets,
+        //            Scopes,
+        //            "user",
+        //            CancellationToken.None,
+        //            new FileDataStore(credPath, true)).Result;
+        //    }
+
+        //    var service = new SheetsService(new BaseClientService.Initializer()
+        //    {
+        //        HttpClientInitializer = credential,
+        //        ApplicationName = ApplicationName,
+        //    });
+
+        //    SpreadsheetsResource.ValuesResource.GetRequest request =
+        //            service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+        //    ValueRange response = request.Execute();
+
+        //    return response.Values;
+        //}
     }
 }
